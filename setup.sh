cddaae45a52933cd1af753dacb23bbcc9232ea02
Prak <myprak@github.com>
2015-10-11 02:35:41 +0700

    Emacs install updated

--- setup.sh
+++ setup.sh
@@ -1,11 +1,4 @@
 
 
-## install apt-get-repository
-sudo apt-get install -y software-properties-common
-
-# Install emacs24
-# https://launchpad.net/~cassou/+archive/emacs
-sudo add-apt-repository -y ppa:cassou/emacs
-sudo apt-get -qq update
-sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
-
+# Install emacs
+sudo apt-get install -y emacs
