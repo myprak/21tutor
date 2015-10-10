2883366007be53e80d49bd03851db861bc4311cb
myprak <myprak@gmail.com>
2015-10-11 01:20:37 +0700

    Added emacs

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 
 
+## install apt-get-repository
+sudo apt-get install -y software-properties-common
 
-sudo apt-get install -y python-software-properties
+# Install emacs24
+# https://launchpad.net/~cassou/+archive/emacs
+sudo add-apt-repository -y ppa:cassou/emacs
+sudo apt-get -qq update
+sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
+
